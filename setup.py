2fa52c8b6684959cdbf212dbee96d71c395c26a1
unknown <drewdudash@gmail.com>
2013-12-10 19:06:44 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+
+from setuptools import setup
+
+setup(
+  name = 'reassure-py',
+  version = '0.0.1',
+  description = 'Error coping library.',
+  author = 'Andrew Dudash',
+  author_email = 'drewdudash@gmail.com',
+  packages = ['reassure-py'],
+
+  long_description = "A program for customizing Python error messages to reduce the mental load of debugging.",
+  keywords = 'joke reassure reassure-py reassure.py humor',
+  license = 'MIT',
+  install_requires = ['setuptools'],
+)
+  
