78b5c6b2f247aa1624f68f5a369ea3de04f6f31a
unknown <drewdudash@gmail.com>
2013-12-11 19:50:19 -0500

    Changed to more oop less global

--- reassure/reassure.py
+++ reassure/reassure.py
@@ -5,35 +5,41 @@ import webbrowser
 
 OLD_DOC = sys.excepthook.__doc__
 
-def doc_check(handler):
+class Hooks(object):
+  def __init__(self, hooks):
+    self.hooks = dict([(hook.__name__, hook) for hook in hooks])
+  def set_hook(self, key):
+    'Sets the new excepthook to the corresponding value.'
+    Hooks._set_hook(self.hooks[key])
+  @staticmethod
+  def _set_hook(handler, do_doc_check = True):
+    "Sets the excepthook to a new value while possibly adding a new __doc__ value."
+    if do_doc_check:
+        handler = Hooks._doc_check(handler)
+    sys.excepthook = handler
+  @staticmethod
+  def _doc_check(handler):
     "Will give the handler the __doc__ value of the original sys.excepthook, if it has a false value for its current __doc__ value. (e.g. \"\", None, False)"
     if not handler.__doc__:
         handler.__doc__ = OLD_DOC
     return handler
-
-def set_hook(handler, do_doc_check = True):
-    "Sets the excepthook to a new value while possibly adding a new __doc__ value."
-    if do_doc_check:
-        handler = doc_check(handler)
-    sys.excepthook = handler
-
-def print_err(msg):
-  sys.stderr.write(msg + '\n')
-
-def print_tb(tb):
-    print_err('Traceback (most recent call last):')
-    traceback.print_tb(tb)
-
-HOOKS = {}
-
-def default_hook_test(exc, value, tb):
+  @staticmethod
+  def base_hook(exc, value, tb):
     "Returns the same result as the default sys.excepthook would."
-    print_tb(tb)
-    print_err("%s: %s" % (exc.__name__, value))
-    
-HOOKS['test1'] = default_hook_test
+    Hooks.print_tb(tb)
+    Hooks.print_err("%s: %s" % (exc.__name__, value))
+  @staticmethod
+  def print_err(msg):
+    sys.stderr.write(msg + '\n')
+  @staticmethod
+  def print_tb(tb):
+      Hooks.print_err('Traceback (most recent call last):')
+      traceback.print_tb(tb)
 
-def _lambda(exc, value, tb):
+    
+
+
+def chipper(exc, value, tb):
     gasp = random.choice([
         'Oh no! ;~;',
         'Uh oh...',
@@ -45,13 +51,12 @@ def _lambda(exc, value, tb):
         'Up and at\'em!',
     ])
     closer = '~~' + closer + '~~'
-    print_err(gasp)
-    default_hook_test(exc, value, tb)
-    print_err(closer)
-HOOKS['chipper'] = _lambda
+    Hooks.print_err(gasp)
+    Hooks.base_hook(exc, value, tb)
+    Hooks.print_err(closer)
 
-def _lambda(exc, value, tb):
-  print_err('''\
+def greenday(exc, value, tb):
+  Hooks.print_err('''\
 I've tried so hard
   And got so far
     But in the end
@@ -61,15 +66,13 @@ I've tried so hard
   But in the end
 It doesn't even matter
 ''')
-  default_hook_test(exc, value, tb)
+  Hooks.base_hook(exc, value, tb)
   greenday_video = 'http://www.youtube.com/watch?v=ZP-bAlOJzWc'
   webbrowser.open(greenday_video)
-HOOKS['greenday'] = _lambda
+
+hooks = Hooks([chipper, greenday])
   
 
 
-def set_hook_by_key(key):
-    'Sets the new excepthook to the corresponding HOOKS value.'
-    set_hook(HOOKS[key])
 
     
